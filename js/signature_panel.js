da5491c84cc776d39d030c77fb9cc4c411800361
Tse-Hsien Chiang <jackblackevo@gmail.com>
2017-02-17 16:44:45 +0800

    refactoring to use mvc

--- js/signature_panel.js
+++ js/signature_panel.js
@@ -1,227 +1,294 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict'
-  var mainCanvas = document.getElementById('mainCanvas');
-  var mainCanvasContext = mainCanvas.getContext('2d');
-  /** 是否繪圖的 flag */
-  var isDrawing = false;
+  var model = {
+    _canvas: null,
+    _isDrawing: false,
+    _isClean: true,
+    _lineColor: 'black',
+    _lineWidth: 1.5,
+    /**
+     * 檢查 fancybox 是否開啟
+     */
+    _checkPopupOpen: function () {
+      return $.fancybox.isOpen;
 
-  var cleanCanvasBtn = document.getElementById('cleanCanvasBtn');
-  var getCanvasImgBtn = document.getElementById('getCanvasImgBtn');
+    },
+    /**
+     * 檢查簽名是否超出 Canvas 區域
+     * 
+     * @param {number} userPositionX 輸入位置（螢幕）水平
+     * @param {number} userPositionY 輸入位置（螢幕）垂直
+     */
+    _checkIsOverRegion: function (userPositionX, userPositionY) {
+      var canvasRect = model._canvas.getBoundingClientRect();
 
-  /**
-   * 開始繪圖
-   * @param {Object} event 事件物件
-   */
-  function startDrawing(event) {
-    event.preventDefault();
+      var xOnCanvas = userPositionX - canvasRect.left;
+      var yOnCanvas = userPositionY - canvasRect.top;
 
-    if (checkPopupOpen()) {
-      return;
+      return ((xOnCanvas < 0 || xOnCanvas > model._canvas.width) || (yOnCanvas < 0 || yOnCanvas > model._canvas.height));
 
-    }
+    },
+    _initCanvas: function () {
+      var canvasContext = model._canvas.getContext('2d');
 
-    isDrawing = true;
+      // 將 Canvas 背景填滿白色
+      canvasContext.fillStyle = 'white';
+      canvasContext.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
+      // 線條設定為 1.5 倍
+      canvasContext.lineWidth = 1.5;
 
-    var targetCanvas = event.target;
-    var targetCanvasContext = targetCanvas.getContext('2d');
-
-    targetCanvasContext.beginPath();
-
-    var eventType = event.type;
-
-    if (eventType === 'touchstart') {
-      targetCanvasContext.moveTo(event.touches.item(0).pageX - targetCanvas.offsetLeft, event.touches.item(0).pageY - targetCanvas.offsetTop);
-    } else {
-      targetCanvasContext.moveTo(event.pageX - targetCanvas.offsetLeft, event.pageY - targetCanvas.offsetTop);
-
-    }
-
-  }
-
-  /**
-   * 檢查是否超出 Canvas 範圍
-   * @param {Object} event 事件物件
-   */
-  function checkIsOverRegion(event) {
-    var targetCanvas = event.target;
-    var targetCanvasRect = event.target.getBoundingClientRect();
-
-    var eventClientX = !isNaN(event.clientX) ? event.clientX : event.touches.item(0).clientX;
-    var eventClientY = !isNaN(event.clientY) ? event.clientY : event.touches.item(0).clientY;
-
-    var nowUserPositionX = eventClientX - targetCanvasRect.left;
-    var nowUserPositionY = eventClientY - targetCanvasRect.top;
-
-    return ((nowUserPositionX < 0 || nowUserPositionX > targetCanvas.width) || (nowUserPositionY < 0 ||
-      nowUserPositionY > targetCanvas.height));
-
-  }
-
-  /**
-   * 清除 Canvas
-   * @param {Object} targetCanvas 目標 Canvas
-   */
-  function cleanCanvas(targetCanvas) {
-    var targetCanvasContext = targetCanvas.getContext('2d');
-    targetCanvasContext.clearRect(0, 0, targetCanvas.width, targetCanvas.height);
-
-  }
-
-  /**
-   * 停止繪圖
-   */
-  function stopDrawing() {
-    isDrawing = false;
-
-  }
-
-  /**
-   * 清除並停止繪圖
-   * @param {Object} event 事件物件
-   */
-  function cleanAndStopDrawing(event) {
-    event.preventDefault();
-
-    var targetCanvas = event.target;
-
-    if (checkIsOverRegion(event) && isDrawing) {
-      alert('請於白色方框內簽名！');
-      cleanCanvas(targetCanvas);
-      stopDrawing();
-
-    }
-
-
-  }
-
-  /**
-   * 執行繪圖
-   * @param {Object} event 事件物件
-   */
-  function draw(event) {
-    cleanAndStopDrawing(event);
-
-    if (isDrawing) {
-      var targetCanvas = event.target;
-      var targetCanvasContext = targetCanvas.getContext('2d');
-
-      var eventType = event.type;
-      var x;
-      var y;
-
-      if (eventType === 'touchmove') {
-        x = event.touches.item(0).pageX - targetCanvas.offsetLeft;
-        y = event.touches.item(0).pageY - targetCanvas.offsetTop;
-
-      } else {
-        x = event.pageX - targetCanvas.offsetLeft;
-        y = event.pageY - targetCanvas.offsetTop;
+    },
+    prepareToDraw: function (canvas, userPositionX, userPositionY) {
+      if (model._checkPopupOpen()) {
+        return;
 
       }
 
-      // 筆畫顏色設定為黑色
-      targetCanvasContext.strokeStyle = 'black';
+      if (model._isClean) {
+        model._canvas = canvas;
 
-      targetCanvasContext.lineTo(x, y);
-      targetCanvasContext.stroke();
+        model._initCanvas();
 
-    }
+      }
 
-  }
+      model._isDrawing = true;
 
-  /**
-   * 檢查 fancybox 是否開啟
-   */
-  function checkPopupOpen() {
-    return $.fancybox.isOpen;
+      var canvasContext = model._canvas.getContext('2d');
 
-  }
 
-  /**
-   * 顯示提示 fancybox
-   */
-  function showPopup() {
-    if (!checkPopupOpen()) {
-      // if (screen.orientation.type === 'portrait-primary') {
-      $.fancybox.open({
-        href: '#msg',
-        type: 'inline',
-        height: '30',
-        width: '30',
-        autoSize: false,
-        closeBtn: false,
-        scrollOutside: false,
-        helpers: {
-          overlay: {
-            closeClick: false
+      canvasContext.beginPath();
+      canvasContext.moveTo(userPositionX - model._canvas.offsetLeft, userPositionY - model._canvas.offsetTop);
+
+    },
+    stopDraw: function () {
+      model._isDrawing = false;
+
+    },
+    draw: function (userPositionX, userPositionY) {
+      if (!model._isDrawing) {
+        return;
+
+      }
+
+      var canvasContext = model._canvas.getContext('2d');
+
+      if (model._checkIsOverRegion(userPositionX, userPositionY)) {
+        model.cleanDraw(true);
+
+      } else {
+        canvasContext.lineTo(userPositionX - model._canvas.offsetLeft, userPositionY - model._canvas.offsetTop);
+        canvasContext.stroke();
+
+        model._isClean = false;
+
+      }
+
+    },
+    cleanDraw: function (isShowAlert) {
+      if (model._canvas === null) {
+        return;
+
+      }
+
+      var canvasContext = model._canvas.getContext('2d');
+
+      model.stopDraw();
+
+      canvasContext.clearRect(0, 0, model._canvas.width, model._canvas.height);
+
+      model._isClean = true;
+
+      if (isShowAlert) {
+        view.showOverRegionAlert();
+
+      }
+
+    },
+    /**
+     * 開啟或關閉 fancybox
+     */
+    switchPopup: function (isOn) {
+      if (isOn) {
+        $.fancybox.open({
+          href: '#msg',
+          type: 'inline',
+          height: '30',
+          width: '30',
+          autoSize: false,
+          closeBtn: false,
+          scrollOutside: false,
+          helpers: {
+            overlay: {
+              closeClick: false
+            }
           }
-        }
-      });
+        });
 
-    } else {
-      $.fancybox.close();
+      } else if (model._checkPopupOpen()) {
+        $.fancybox.close();
 
-    }
+      }
 
-  }
+    },
+    downloadCanvasImg: function () {
+      if (model._isClean) {
+        return;
 
-  /**
-   * 檢查是否為橫式螢幕
-   */
-  function checkIsLandscape() {
-    var query = window.matchMedia('(orientation:landscape)');
-    return query.matches;
+      }
 
-  }
+      var downloadUrl = model._canvas.toDataURL('image/png');
 
-  // 加入事件處理
-  mainCanvas.addEventListener('mousedown', startDrawing);
-  mainCanvas.addEventListener('mouseup', stopDrawing);
-  mainCanvas.addEventListener('mouseout', cleanAndStopDrawing);
-  mainCanvas.addEventListener('mousemove', draw);
+      var downloadLink = document.createElement('a');
+      downloadLink.href = downloadUrl;
+      downloadLink.download = 'signature.png';
 
-  mainCanvas.addEventListener('touchstart', startDrawing);
-  mainCanvas.addEventListener('touchend', stopDrawing);
-  mainCanvas.addEventListener('touchmove', draw);
-
-  cleanCanvasBtn.addEventListener('click', function () {
-    cleanCanvas(mainCanvas);
-
-  });
-  getCanvasImgBtn.addEventListener('click', function () {
-    var downloadUrl = mainCanvas.toDataURL('image/png');
-
-    var downloadLink = document.createElement('a');
-    downloadLink.href = downloadUrl;
-    downloadLink.download = 'signature.png';
-
-    downloadLink.click();
-
-
-  });
-
-  window.addEventListener('orientationchange', function () {
-    if (!checkIsLandscape()) {
-      showPopup();
-
-    } else {
-      $.fancybox.close();
+      downloadLink.click();
 
     }
+  };
 
-  });
+  var view = {
+    mainCanvas: document.getElementById('mainCanvas'),
+    cleanCanvasBtn: document.getElementById('cleanCanvasBtn'),
+    getCanvasImgBtn: document.getElementById('getCanvasImgBtn'),
+    showOverRegionAlert: function () {
+      alert('請於白色方框內簽名！');
 
-  // 頁面載入完畢即檢查螢幕方向
-  if (!checkIsLandscape()) {
-    showPopup();
+    }
+  };
 
-  }
+  var controller = {
+    checkIsLandscape: function () {
+      var query = window.matchMedia('(orientation:landscape)');
 
-  // 將 Canvas 背景填滿白色
-  mainCanvasContext.fillStyle = 'white';
-  mainCanvasContext.fillRect(0, 0, mainCanvas.width, mainCanvas.height);
+      model.switchPopup(!query.matches);
 
-  // 線條設定為 1.5 倍
-  mainCanvasContext.lineWidth = 1.5;
+    },
+    startDrawing: function (event) {
+      event.preventDefault();
+
+      var targetCanvas = event.target;
+      var userX;
+      var userY;
+
+      var eventType = event.type;
+
+      switch (eventType) {
+        case 'touchstart':
+          userX = event.touches.item(0).pageX;
+          userY = event.touches.item(0).pageY;
+
+          model.prepareToDraw(targetCanvas, userX, userY);
+
+          break;
+
+        case 'mousedown':
+          userX = event.pageX;
+          userY = event.pageY;
+
+          model.prepareToDraw(targetCanvas, userX, userY);
+
+          break;
+
+      }
+
+    },
+    stopDrawing: function () {
+      model.stopDraw();
+
+    },
+    drawing: function (event) {
+      event.preventDefault();
+
+      var userX;
+      var userY;
+
+      var eventType = event.type;
+
+      switch (eventType) {
+        case 'touchmove':
+          userX = event.touches.item(0).pageX;
+          userY = event.touches.item(0).pageY;
+
+          model.draw(userX, userY);
+
+          break;
+
+        case 'mousemove':
+          userX = event.pageX;
+          userY = event.pageY;
+
+          model.draw(userX, userY);
+
+          break;
+
+      }
+
+    },
+    cleanDrawing: function () {
+      model.cleanDraw();
+
+    },
+    getSignatureImg: function () {
+      model.downloadCanvasImg();
+
+    }
+  };
+
+  (function pageInit() {
+    window.addEventListener('orientationchange', function () {
+      controller.checkIsLandscape();
+
+    });
+
+    view.mainCanvas.addEventListener('touchstart', function (event) {
+      controller.startDrawing(event);
+
+    });
+
+    view.mainCanvas.addEventListener('touchend', function () {
+      controller.stopDrawing();
+
+    });
+
+    view.mainCanvas.addEventListener('touchmove', function (event) {
+      controller.drawing(event);
+
+    });
+
+    view.mainCanvas.addEventListener('mousedown', function (event) {
+      controller.startDrawing(event);
+
+    });
+
+    view.mainCanvas.addEventListener('mouseup', function () {
+      controller.stopDrawing();
+
+    });
+
+    view.mainCanvas.addEventListener('mouseout', function () {
+      controller.cleanDrawing();
+
+    });
+
+    view.mainCanvas.addEventListener('mousemove', function (event) {
+      controller.drawing(event);
+
+    });
+
+    view.cleanCanvasBtn.addEventListener('click', function () {
+      controller.cleanDrawing();
+
+    });
+
+    view.getCanvasImgBtn.addEventListener('click', function () {
+      controller.getSignatureImg();
+
+    });
+
+    // 頁面載入完畢即檢查螢幕方向
+    controller.checkIsLandscape();
+
+  })();
 
 });
